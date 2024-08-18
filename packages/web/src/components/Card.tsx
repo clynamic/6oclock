97abf209078022a6e759a56103ec7ac75c34bd95
clragon <clragon@clynamic.net>
2024-08-18 11:14:54 +0200

    added files

---
+++ packages/web/src/components/Card.tsx
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const Card = styled.div`
+  background-color: var(--surface);
+  color: var(--on-surface);
+  border-radius: var(--container-radius);
+  box-shadow: var(--shadow);
+  padding: var(--spacing-md);
+`;
