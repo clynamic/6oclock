6ec9cb8daa3544c6fd40a363c35364b653fa55a4
clragon <clragon@clynamic.net>
2024-08-21 19:52:38 +0200

    added more api endpoints

--- packages/web/src/api/index.ts
+++ packages/web/src/api/index.ts
@@ -3,3 +3,6 @@ export * from "./tickets";
 export * from "./user-feedbacks";
 export * from "./model";
 export * from "./posts";
+export * from "./approvals";
+export * from "./uploads";
+export * from "./post-flags";
