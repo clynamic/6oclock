0af32677ef97a5106e989079dd7a1dcb50fb30f5
clragon <clragon@clynamic.net>
2024-08-21 19:01:32 +0200

    added grid placeholder styles

--- packages/web/src/dashboard/DashboardGrid.tsx
+++ packages/web/src/dashboard/DashboardGrid.tsx
@@ -1,4 +1,4 @@
-import { useTheme } from "@mui/material";
+import { Box, useTheme } from "@mui/material";
 import {
   Responsive,
   ResponsiveProps,
@@ -19,15 +19,24 @@ export const DashboardGrid: React.FC<DashboardGridProps> = ({ ...rest }) => {
   const { breakpoints } = useTheme();
 
   return (
-    <ResponsiveGridLayout
-      draggableHandle=".react-draggable-handle"
-      // TODO: fix this
-      // resizeHandle={(handle) => <ResizableHandle direction={handle} />}
-      compactType={"horizontal"}
-      breakpoints={breakpoints.values}
-      cols={{ xl: 16, lg: 12, md: 9, sm: 6, xs: 4 }}
-      rowHeight={50}
-      {...rest}
-    />
+    <Box
+      sx={{
+        ".react-grid-item.react-grid-placeholder": {
+          borderRadius: 1,
+          backgroundColor: "secondary.light",
+        },
+      }}
+    >
+      <ResponsiveGridLayout
+        draggableHandle=".react-draggable-handle"
+        // TODO: fix this
+        // resizeHandle={(handle) => <ResizableHandle direction={handle} />}
+        compactType={"horizontal"}
+        breakpoints={breakpoints.values}
+        cols={{ xl: 16, lg: 12, md: 9, sm: 6, xs: 4 }}
+        rowHeight={50}
+        {...rest}
+      />
+    </Box>
   );
 };
