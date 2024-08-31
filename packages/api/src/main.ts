682faf4d0cf69beed132c92a28963b04939067cf
clragon <clragon@clynamic.net>
2024-08-31 02:48:22 +0200

    added local CORD

--- packages/api/src/main.ts
+++ packages/api/src/main.ts
@@ -1,4 +1,5 @@
 import { ValidationPipe } from '@nestjs/common';
+import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
 import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app/app.module';
@@ -12,7 +13,27 @@ async function bootstrap() {
       transform: true,
     }),
   );
+
   DocsModule.setupSwagger(app);
+
+  const corsOptions: CorsOptions = {
+    origin: (origin, callback) => {
+      const regexes = [
+        /^(https?:\/\/)?(localhost)(:\d+)?(\/.*)?$/,
+        /^(https?:\/\/)?(127\.\d+\.\d+\.\d+)(:\d+)?(\/.*)?$/,
+        /^(https?:\/\/)?(0:0:0:0:0:0:0:1)(:\d+)?(\/.*)?$/,
+      ];
+
+      if (!origin || regexes.some((regex) => regex.test(origin))) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
+    credentials: true,
+  };
+
+  app.enableCors(corsOptions);
 
   await app.listen(3000);
 }
