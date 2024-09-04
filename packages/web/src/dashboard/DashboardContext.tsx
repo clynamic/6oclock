ae14f248992fc7197268cf18f5f4da0ffe464533
clragon <clragon@clynamic.net>
2024-09-04 13:23:01 +0200

    added editing dashboard

--- packages/web/src/dashboard/DashboardContext.tsx
+++ packages/web/src/dashboard/DashboardContext.tsx
@@ -1,9 +1,11 @@
 /* eslint-disable react-refresh/only-export-components */
+import { Breakpoint, useTheme } from "@mui/material";
 import React, {
   createContext,
   useCallback,
   useContext,
   useEffect,
+  useMemo,
   useState,
 } from "react";
 
@@ -14,9 +16,17 @@ import {
   useDashboard as useRemoteDashboard,
   useUpdateDashboard,
 } from "../api";
+import { useCurrentBreakpoint } from "../utils";
+import { useCurrentLayout } from "./current-layout";
+import { DashboardLayout, DashboardLayouts } from "./DashboardGrid";
+import { buildCatalogLayout, DashboardCatalog } from "./DashboardItem";
 
 interface DashboardContextType {
   config?: DashboardConfig;
+  layouts?: DashboardLayouts;
+  currentBreakpoint?: Breakpoint;
+  currentLayout?: DashboardLayout[];
+  catalog: DashboardCatalog;
   isLoading?: boolean;
   isError?: boolean;
   error?: unknown;
@@ -32,12 +42,14 @@ const DashboardContext = createContext<DashboardContextType | undefined>(
 
 export interface DashboardProviderProps {
   type: DashboardConfigType;
+  catalog: DashboardCatalog;
   children: React.ReactNode;
 }
 
 export const DashboardProvider: React.FC<DashboardProviderProps> = ({
   children,
   type,
+  catalog,
 }) => {
   const { data, isLoading, isError, error } = useRemoteDashboard(type);
   const { mutateAsync } = useUpdateDashboard();
@@ -60,10 +72,37 @@ export const DashboardProvider: React.FC<DashboardProviderProps> = ({
     [mutateAsync, type]
   );
 
+  const {
+    breakpoints: { keys: breakpoints },
+  } = useTheme();
+
+  const layouts: DashboardLayouts = useMemo(
+    () =>
+      breakpoints.reduce((acc, breakpoint) => {
+        return {
+          ...acc,
+          [breakpoint]: config?.positions[breakpoint]
+            ? config.positions[breakpoint].map<DashboardLayout>((position) => ({
+                ...catalog[position.i].defaultLayout[breakpoint],
+                ...position,
+              }))
+            : buildCatalogLayout(catalog, breakpoint),
+        };
+      }, {} as DashboardLayouts),
+    [breakpoints, catalog, config]
+  );
+
+  const currentBreakpoint = useCurrentBreakpoint();
+  const currentLayout = useCurrentLayout(layouts);
+
   return (
     <DashboardContext.Provider
       value={{
         config,
+        layouts,
+        currentBreakpoint,
+        currentLayout,
+        catalog,
         isLoading,
         isError,
         error,
