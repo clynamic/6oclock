44d8b8b9a709cb513f369127f4675801fea55f63
clragon <clragon@clynamic.net>
2024-09-05 10:05:05 +0200

    added better mobile nav (WIP)

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,9 +1,10 @@
 import { CssBaseline, ThemeProvider } from "@mui/material";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { useMemo } from "react";
-import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 import { AuthGuard, AuthProvider } from "../auth";
+import { HomePage } from "../home";
 import { JanitorDashboard } from "../janitors/dashboard/JanitorDashboard";
 import { LoginPage } from "../login";
 import { ModDashboardPage } from "../mods";
@@ -24,7 +25,7 @@ export const App: React.FC = () => {
               <Route path="/login" element={<LoginPage />} />
 
               <Route path="/" element={<AuthGuard />}>
-                <Route path="/" element={<Navigate to="/mods" />} />
+                <Route path="/" element={<HomePage />} />
                 <Route path="/mods" element={<ModDashboardPage />} />
                 <Route path="/mods/tickets" element={<TicketerPage />} />
                 <Route path="/janitors" element={<JanitorDashboard />} />
