4ccea551b331242e43db2a53c0178ff2b2015023
clragon <clragon@clynamic.net>
2024-09-09 22:04:29 +0200

    fixed dashboard init times

--- packages/web/src/janitors/dashboard/catalog.tsx
+++ packages/web/src/janitors/dashboard/catalog.tsx
@@ -5,16 +5,25 @@ import { ApprovalActivityChart } from "../charts";
 export const janitorDashboardCatalog: DashboardCatalog = {
   approvals: {
     component: ApprovalLeaderboard,
-    defaultLayout: createSimpleLayout({
-      x: 0,
-      y: 0,
-      w: 4,
-      h: 11,
-      minW: 3,
-      maxW: 6,
-      minH: 9,
-      maxH: 20,
-    }),
+    defaultLayout: createSimpleLayout(
+      {
+        x: 0,
+        y: 0,
+        w: 4,
+        h: 11,
+        minW: 3,
+        maxW: 6,
+        minH: 9,
+        maxH: 20,
+      },
+      {
+        xs: { x: 0, y: 0, w: 4, h: 11 },
+        sm: { x: 0, y: 0, w: 4, h: 11 },
+        md: { x: 0, y: 0, w: 4, h: 11 },
+        lg: { w: 4, h: 11, x: 0, y: 0 },
+        xl: { x: 0, y: 0, w: 6, h: 11 },
+      }
+    ),
     card: {
       title: "Approvals",
       variant: "outlined",
@@ -22,15 +31,24 @@ export const janitorDashboardCatalog: DashboardCatalog = {
   },
   activity: {
     component: ApprovalActivityChart,
-    defaultLayout: createSimpleLayout({
-      x: 0,
-      y: 11,
-      w: 8,
-      h: 4,
-      minW: 4,
-      minH: 3,
-      maxH: 9,
-    }),
+    defaultLayout: createSimpleLayout(
+      {
+        x: 0,
+        y: 11,
+        w: 8,
+        h: 4,
+        minW: 4,
+        minH: 3,
+        maxH: 9,
+      },
+      {
+        xs: { x: 0, y: 11, w: 8, h: 4 },
+        sm: { x: 0, y: 11, w: 8, h: 4 },
+        md: { x: 0, y: 11, w: 8, h: 4 },
+        lg: { w: 8, h: 4, x: 4, y: 0 },
+        xl: { x: 6, y: 0, w: 10, h: 5 },
+      }
+    ),
     card: { title: "Activity" },
   },
 };
