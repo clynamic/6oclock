354003b937bff620041d90004ee9fc99e1d96b5b
clragon <clragon@clynamic.net>
2024-09-09 03:52:15 +0200

    improved home page code

--- packages/web/src/home/HomePage.tsx
+++ packages/web/src/home/HomePage.tsx
@@ -1,5 +1,12 @@
 import { Beenhere, Shield } from "@mui/icons-material";
-import { Button, Divider, Stack, Typography } from "@mui/material";
+import {
+  Button,
+  Divider,
+  Stack,
+  Theme,
+  ThemeProvider,
+  Typography,
+} from "@mui/material";
 import { useNavigate } from "react-router-dom";
 
 import { Page, PageBody, PageFooter, PageHeader, WindowTitle } from "../page";
@@ -23,46 +30,62 @@ export const HomePage = () => {
               sm: 6,
             }}
           >
-            <Button
-              sx={{
-                width: "200px",
-                height: "200px",
-                maxWidth: "100%",
-                maxHeight: "100%",
-                border: (theme) => `2px solid ${theme.palette.secondary.main}`,
-                boxShadow: (theme) => theme.shadows[6],
-              }}
-              variant="text"
-              color="secondary"
-              onClick={() => {
-                navigate("/mods");
-              }}
+            <ThemeProvider<Theme>
+              theme={(theme) => ({
+                ...theme,
+                components: {
+                  MuiButton: {
+                    defaultProps: {
+                      variant: "text",
+                      color: "secondary",
+                    },
+                    styleOverrides: {
+                      root: {
+                        width: "200px",
+                        height: "200px",
+                        maxWidth: "100%",
+                        maxHeight: "100%",
+                        border: `2px solid ${theme.palette.secondary.main}`,
+                        boxShadow: theme.shadows[6],
+                      },
+                    },
+                  },
+                },
+              })}
             >
-              <Stack direction="row" alignItems="center" spacing={1}>
-                <Shield sx={{ fontSize: 32 }} />
-                <Typography variant="h5">Mods</Typography>
-              </Stack>
-            </Button>
-            <Button
-              sx={{
-                width: "200px",
-                height: "200px",
-                maxWidth: "100%",
-                maxHeight: "100%",
-                border: (theme) => `2px solid ${theme.palette.secondary.main}`,
-                boxShadow: (theme) => theme.shadows[6],
-              }}
-              variant="text"
-              color="secondary"
-              onClick={() => {
-                navigate("/janitors");
-              }}
-            >
-              <Stack direction="row" alignItems="center" spacing={1}>
-                <Beenhere sx={{ fontSize: 32 }} />
-                <Typography variant="h5">Janitors</Typography>
-              </Stack>
-            </Button>
+              {/*
+                <Button
+                  onClick={() => {
+                    navigate("/admins");
+                  }}
+                >
+                  <Stack direction="row" alignItems="center" spacing={1}>
+                    <Gavel sx={{ fontSize: 32 }} />
+                    <Typography variant="h5">Admins</Typography>
+                  </Stack>
+                </Button>
+              */}
+              <Button
+                onClick={() => {
+                  navigate("/mods");
+                }}
+              >
+                <Stack direction="row" alignItems="center" spacing={1}>
+                  <Shield sx={{ fontSize: 32 }} />
+                  <Typography variant="h5">Mods</Typography>
+                </Stack>
+              </Button>
+              <Button
+                onClick={() => {
+                  navigate("/janitors");
+                }}
+              >
+                <Stack direction="row" alignItems="center" spacing={1}>
+                  <Beenhere sx={{ fontSize: 32 }} />
+                  <Typography variant="h5">Janitors</Typography>
+                </Stack>
+              </Button>
+            </ThemeProvider>
           </Stack>
           <Divider orientation="horizontal" />
           <Typography variant="h4">{`6 o'clock`}</Typography>
