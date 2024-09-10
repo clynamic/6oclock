2c23f3812370470122e78fe0efa236f611af8dd3
clragon <clragon@clynamic.net>
2024-09-10 03:52:22 +0200

    added db setup

--- packages/api/src/app/config.module.ts
+++ packages/api/src/app/config.module.ts
@@ -5,7 +5,6 @@ import Joi from 'joi';
 export enum AppConfigKeys {
   E621_GLOBAL_USERNAME = 'E621_GLOBAL_USERNAME',
   E621_GLOBAL_API_KEY = 'E621_GLOBAL_API_KEY',
-  DATABASE_URL = 'DATABASE_URL',
   JWT_SECRET_FILE = 'JWT_SECRET_FILE',
   CORS_ALLOWED_ORIGINS = 'CORS_ALLOWED_ORIGINS',
 }
@@ -18,7 +17,6 @@ export enum AppConfigKeys {
       validationSchema: Joi.object<Record<AppConfigKeys, Joi.Schema>>({
         E621_GLOBAL_USERNAME: Joi.string().required(),
         E621_GLOBAL_API_KEY: Joi.string().required(),
-        DATABASE_URL: Joi.string().optional(),
         JWT_SECRET_FILE: Joi.string().optional(),
         CORS_ALLOWED_ORIGINS: Joi.alternatives()
           .try(Joi.string(), Joi.array().items(Joi.string()))
