9dc0f111529453dfa0301a7d9910268859375b63
clragon <clragon@clynamic.net>
2024-09-10 05:05:43 +0200

    fixed database entities

--- packages/api/src/data-source.ts
+++ packages/api/src/data-source.ts
@@ -4,8 +4,8 @@ import { SnakeNamingStrategy } from 'typeorm-naming-strategies';
 export const AppDataSourceOptions: DataSourceOptions = {
   type: 'sqlite',
   database: 'db.sqlite',
-  entities: ['**/*.entity{.ts,.js}'],
-  migrations: ['src/migrations/*{.ts,.js}'],
+  entities: ['**/*.entity.js'],
+  migrations: ['src/migrations/*.js'],
   migrationsRun: process.env['NODE_ENV'] === 'production',
   synchronize: process.env['NODE_ENV'] !== 'production',
   namingStrategy: new SnakeNamingStrategy(),
