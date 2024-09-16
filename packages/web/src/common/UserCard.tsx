8995ac8c485e6ec22a9eaa5731e7a1c8cdc9da0a
clragon <clragon@clynamic.net>
2024-09-16 16:43:15 +0200

    added mod profile card (WIP)

---
+++ packages/web/src/common/UserCard.tsx
@@ -0,0 +1,22 @@
+import { Stack } from "@mui/material";
+
+import { useUserHead } from "../api";
+import { useChartParamsValue } from "../utils";
+import { UserAvatar } from "./UserAvatar";
+import { UsernameText } from "./UsernameText";
+
+export const UserCard: React.FC = () => {
+  const { userId } = useChartParamsValue();
+  const { data: user } = useUserHead(userId ?? 0, {
+    query: {
+      enabled: !!userId,
+    },
+  });
+
+  return (
+    <Stack direction="row" spacing={2} alignItems="center">
+      <UserAvatar user={user} shape="rounded" size={64} />
+      <UsernameText user={user} />
+    </Stack>
+  );
+};
