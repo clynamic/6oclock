3012fe2d3a6c7e879d74f4c1f2936889ac59fcf5
clragon <clragon@clynamic.net>
2024-09-16 16:43:15 +0200

    added mod profile card (WIP)

--- src/mods/profile/ModProfileCatalog.tsx
+++ src/mods/profile/ModProfileCatalog.tsx
@@ -1,26 +1,51 @@
+import { UserCard } from "../../common";
 import { createSimpleLayout, DashboardCatalog } from "../../dashboard";
 import { TicketHandlerChart } from "../charts";
 
 export const modProfileCatalog: DashboardCatalog = {
-  ticketsClosed: {
-    name: "Tickets Closed",
+  userHead: {
+    name: "User Head",
     defaultLayout: createSimpleLayout(
       {
         x: 0,
         y: 0,
         w: 5,
-        h: 5,
-        minW: 3,
+        h: 2,
+        minW: 2,
         maxW: 5,
-        minH: 4,
-        maxH: 9,
+        minH: 2,
+        maxH: 3,
       },
       {
-        xs: { w: 4, h: 5, x: 0, y: 0 },
-        sm: { w: 4, h: 5, x: 0, y: 0 },
-        md: { w: 5, h: 5, x: 4, y: 0 },
-        lg: { w: 5, h: 5, x: 4, y: 0 },
-        xl: { w: 5, h: 5, x: 6, y: 0 },
+        xs: { w: 4, h: 2, x: 0, y: 0 },
+        sm: { w: 4, h: 2, x: 0, y: 0 },
+        md: { w: 5, h: 2, x: 0, y: 0 },
+        lg: { w: 5, h: 2, x: 0, y: 0 },
+        xl: { w: 5, h: 2, x: 0, y: 0 },
+      }
+    ),
+    card: { variant: "outlined" },
+    component: UserCard,
+  },
+  ticketsClosed: {
+    name: "Tickets Closed",
+    defaultLayout: createSimpleLayout(
+      {
+        x: 0,
+        y: 2,
+        w: 5,
+        h: 5,
+        minW: 2,
+        maxW: 5,
+        minH: 2,
+        maxH: 5,
+      },
+      {
+        xs: { w: 4, h: 5, x: 0, y: 2 },
+        sm: { w: 4, h: 5, x: 0, y: 2 },
+        md: { w: 5, h: 5, x: 0, y: 2 },
+        lg: { w: 5, h: 5, x: 0, y: 2 },
+        xl: { w: 5, h: 5, x: 0, y: 2 },
       }
     ),
     card: { title: "Closed" },
