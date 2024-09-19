1842d726fc23e5162634f3f966812d0037eedd6a
clragon <clragon@clynamic.net>
2024-09-19 17:45:12 +0200

    added deletion flag series

--- packages/api/src/flag/metric/flag-metric.controller.ts
+++ packages/api/src/flag/metric/flag-metric.controller.ts
@@ -1,8 +1,15 @@
-import { Controller, UseGuards } from '@nestjs/common';
-import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
+import { Controller, Get, Param, Query, UseGuards } from '@nestjs/common';
+import {
+  ApiBearerAuth,
+  ApiOperation,
+  ApiResponse,
+  ApiTags,
+} from '@nestjs/swagger';
 import { AuthLevel, RolesGuard } from 'src/auth/auth.guard';
 import { UserLevel } from 'src/auth/auth.level';
+import { PartialDateRange } from 'src/utils';
 
+import { PostDeletedPoint, PostDeletedUserQuery } from './flag-metric.dto';
 import { FlagMetricService } from './flag-metric.service';
 
 @ApiTags('Flags')
@@ -12,4 +19,42 @@ import { FlagMetricService } from './flag-metric.service';
 @ApiBearerAuth()
 export class FlagMetricController {
   constructor(private readonly flagMetricService: FlagMetricService) {}
+
+  @Get('deletion/series')
+  @ApiOperation({
+    summary: 'Post deletion series',
+    description:
+      'Get a time series of post deletion counts for a given date range',
+    operationId: 'getDeletionSeries',
+  })
+  @ApiResponse({
+    status: 200,
+    type: [PostDeletedPoint],
+  })
+  async deletionSeries(
+    @Query() range?: PartialDateRange,
+  ): Promise<PostDeletedPoint[]> {
+    return this.flagMetricService.deletionSeries(range);
+  }
+
+  @Get('deletion/series/:userId')
+  @ApiOperation({
+    summary: 'Post deletion series by user',
+    description:
+      'Get a time series of post deletion counts for a given date range by user',
+    operationId: 'getDeletionSeriesByUser',
+  })
+  @ApiResponse({
+    status: 200,
+    type: [PostDeletedPoint],
+  })
+  async deletionSeriesByUser(
+    @Param('userId') userId: number,
+    @Query() range?: PartialDateRange,
+  ): Promise<PostDeletedPoint[]> {
+    return this.flagMetricService.deletionSeries(
+      range,
+      new PostDeletedUserQuery({ creatorId: userId }),
+    );
+  }
 }
--- packages/api/src/health/health.module.ts
+++ packages/api/src/health/health.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ApprovalEntity } from 'src/approval/approval.entity';
+import { FlagEntity } from 'src/flag/flag.entity';
 import { ManifestEntity } from 'src/manifest/manifest.entity';
 import { TicketEntity } from 'src/ticket/ticket.entity';
 
@@ -9,7 +10,12 @@ import { HealthService } from './health.service';
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([ManifestEntity, TicketEntity, ApprovalEntity]),
+    TypeOrmModule.forFeature([
+      ManifestEntity,
+      TicketEntity,
+      ApprovalEntity,
+      FlagEntity,
+    ]),
   ],
   controllers: [HealthController],
   providers: [HealthService],
