0f2e7ce5725140f4244b617d0fc24eb48be2088e
clragon <clragon@clynamic.net>
2024-10-26 20:05:15 +0200

    moved migration folder

--- packages/api/src/data-source.ts
+++ packages/api/src/data-source.ts
@@ -5,7 +5,7 @@ export const AppDataSourceOptions: DataSourceOptions = {
   type: 'sqlite',
   database: 'db.sqlite',
   entities: ['**/*.entity.js'],
-  migrations: ['src/migrations/*.js'],
+  migrations: ['src/migration/*.js'],
   migrationsRun: process.env['NODE_ENV'] === 'production',
   synchronize: process.env['NODE_ENV'] !== 'production',
   namingStrategy: new SnakeNamingStrategy(),
