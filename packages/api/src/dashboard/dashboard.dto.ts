01e29abde92a88d3a321c25ecd580d77f79b089e
clragon <clragon@clynamic.net>
2024-11-16 00:49:38 +0100

    added dashboard versioning

--- packages/api/src/dashboard/dashboard.dto.ts
+++ packages/api/src/dashboard/dashboard.dto.ts
@@ -1,3 +1,5 @@
+import { Raw } from 'src/common';
+
 import {
   DashboardMeta,
   DashboardPositions,
@@ -5,19 +7,23 @@ import {
 } from './dashboard.entity';
 
 export class DashboardConfig {
-  constructor(partial?: Partial<DashboardConfig>) {
-    if (partial) {
-      Object.assign(this, partial);
-    }
+  constructor(value: Raw<DashboardConfig>) {
+    Object.assign(this, value);
   }
 
   type: DashboardType;
   userId: number;
+  version: number;
   positions: DashboardPositions;
   meta: DashboardMeta;
 }
 
 export class DashboardUpdate {
+  constructor(value: Raw<DashboardUpdate>) {
+    Object.assign(this, value);
+  }
+
+  version?: number;
   positions?: DashboardPositions;
   meta?: DashboardMeta;
 }
