4a79460cd26f3064e1eb5838493c1d5bffae5276
clragon <clragon@clynamic.net>
2024-11-15 20:50:18 +0100

    added error boundary

---
+++ packages/web/src/common/ErrorBoundary.tsx
@@ -0,0 +1,65 @@
+import { Box, Stack, Typography } from '@mui/material';
+import React, { PropsWithChildren } from 'react';
+
+interface ErrorBoundaryState {
+  hasError: boolean;
+}
+
+class ErrorBoundary extends React.Component<
+  PropsWithChildren,
+  ErrorBoundaryState
+> {
+  constructor(props: PropsWithChildren) {
+    super(props);
+    this.state = { hasError: false };
+  }
+
+  static getDerivedStateFromError(): ErrorBoundaryState {
+    return { hasError: true };
+  }
+
+  componentDidCatch(error: Error, errorInfo: React.ErrorInfo) {
+    console.error('Error caught by Error Boundary:', error, errorInfo);
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return (
+        <Box
+          sx={{
+            height: '100%',
+            width: '100%',
+
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+
+            borderRadius: '4px',
+
+            textAlign: 'center',
+            textShadow: '0 0 12px black',
+
+            background: `repeating-linear-gradient(
+              45deg,
+              red,
+              red 16px,
+              transparent 16px,
+              transparent 48px
+            )`,
+          }}
+        >
+          <Stack spacing={1}>
+            <Typography variant="h6">{'Something went wrong :('}</Typography>
+            <Typography variant="body1">
+              Please inform us about this issue.
+            </Typography>
+          </Stack>
+        </Box>
+      );
+    }
+
+    return this.props.children;
+  }
+}
+
+export default ErrorBoundary;
