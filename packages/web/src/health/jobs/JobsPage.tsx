c62ffd70e88b1aff87ab2b0671767598dad5f522
clragon <clragon@clynamic.net>
2024-11-15 22:44:43 +0100

    added jobs page

---
+++ packages/web/src/health/jobs/JobsPage.tsx
@@ -0,0 +1,41 @@
+import { Box, Stack } from '@mui/material';
+
+import { useJobsInfinite } from '../../api';
+import { LoadMoreHint, QueryHint } from '../../common';
+import { Page, PageBody, PageFooter, PageHeader, PageTitle } from '../../page';
+import { JobsFrame } from './JobsFrame';
+
+export const JobsPage: React.FC = () => {
+  const { data, ...query } = useJobsInfinite(undefined, {
+    query: {
+      refetchInterval: 1000 * 60 * 5,
+      initialPageParam: 1,
+      getNextPageParam: (lastPage, _, i) => {
+        if (lastPage.length === 0) {
+          return undefined;
+        }
+        return (i ?? 1) + 1;
+      },
+    },
+  });
+
+  return (
+    <Page>
+      <PageTitle subtitle="Jobs" />
+      <PageHeader />
+      <PageBody>
+        <Box sx={{ width: '100%', maxWidth: 600, margin: 'auto', p: 2 }}>
+          <QueryHint isLoading={query.isLoading} error={query.error}>
+            <Stack sx={{ height: '100%', width: '100%', gap: 1 }}>
+              {data?.pages
+                .flat()
+                .map((item, i) => <JobsFrame key={i} job={item} />)}
+            </Stack>
+          </QueryHint>
+          <LoadMoreHint query={query} />
+        </Box>
+      </PageBody>
+      <PageFooter />
+    </Page>
+  );
+};
