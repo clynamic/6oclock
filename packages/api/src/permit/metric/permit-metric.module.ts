11abe485c264630144f16627c04bf2f023f5c8ff
clragon <clragon@clynamic.net>
2024-11-15 22:17:55 +0100

    added permit metrics

---
+++ packages/api/src/permit/metric/permit-metric.module.ts
@@ -0,0 +1,14 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { PostVersionEntity } from 'src/post_version/post_version.entity';
+
+import { PermitEntity } from '../permit.entity';
+import { PermitMetricController } from './permit-metric.controller';
+import { PermitMetricService } from './permit-metric.service';
+
+@Module({
+  imports: [TypeOrmModule.forFeature([PermitEntity, PostVersionEntity])],
+  controllers: [PermitMetricController],
+  providers: [PermitMetricService],
+})
+export class PermitMetricModule {}
