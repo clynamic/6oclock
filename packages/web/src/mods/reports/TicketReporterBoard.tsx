67e843e281dac2d5759f5eb7ecdac5757d8e3df7
clragon <clragon@clynamic.net>
2024-11-16 01:20:21 +0100

    improved leaderboard errors

--- packages/web/src/mods/reports/TicketReporterBoard.tsx
+++ packages/web/src/mods/reports/TicketReporterBoard.tsx
@@ -3,14 +3,14 @@ import { Button, Stack } from '@mui/material';
 import { Link } from 'react-router-dom';
 
 import { useTicketReporterSummary } from '../../api';
-import { LimitedList, NoDataHint } from '../../common';
+import { LimitedList, QueryHint } from '../../common';
 import { refetchQueryOptions, useChartDateRange } from '../../utils';
 import { TicketReporterFrame } from './TicketReporterFrame';
 
 export const TicketReporterBoard: React.FC = () => {
   const range = useChartDateRange();
 
-  const { data } = useTicketReporterSummary(
+  const { data, isLoading, error } = useTicketReporterSummary(
     {
       ...range,
       limit: 10,
@@ -18,30 +18,33 @@ export const TicketReporterBoard: React.FC = () => {
     refetchQueryOptions(),
   );
 
-  if (data?.length === 0) return <NoDataHint />;
-
   return (
-    <LimitedList
-      indicator={() => (
-        <Stack direction="row" justifyContent="flex-end">
-          <Button
-            size="small"
-            endIcon={<ArrowForward />}
-            component={Link}
-            to="/mods/reports"
-          >
-            See All
-          </Button>
-        </Stack>
-      )}
+    <QueryHint
+      data={data}
+      isLoading={isLoading}
+      error={error}
+      skeleton={Array.from({ length: 5 }).map((_, index) => (
+        <TicketReporterFrame key={index} />
+      ))}
     >
-      {data
-        ? data.map((user) => {
-            return <TicketReporterFrame key={user.userId} summary={user} />;
-          })
-        : Array.from({ length: 5 }).map((_, index) => (
-            <TicketReporterFrame key={index} />
-          ))}
-    </LimitedList>
+      <LimitedList
+        indicator={() => (
+          <Stack direction="row" justifyContent="flex-end">
+            <Button
+              size="small"
+              endIcon={<ArrowForward />}
+              component={Link}
+              to="/mods/reports"
+            >
+              See All
+            </Button>
+          </Stack>
+        )}
+      >
+        {data?.map((user) => {
+          return <TicketReporterFrame key={user.userId} summary={user} />;
+        })}
+      </LimitedList>
+    </QueryHint>
   );
 };
--- packages/web/src/mods/tickets/TicketerBoard.tsx
+++ packages/web/src/mods/tickets/TicketerBoard.tsx
@@ -3,14 +3,14 @@ import { Button, Stack } from '@mui/material';
 import { Link } from 'react-router-dom';
 
 import { useTicketHandlerSummary } from '../../api';
-import { LimitedList, NoDataHint } from '../../common';
+import { LimitedList, QueryHint } from '../../common';
 import { refetchQueryOptions, useChartDateRange } from '../../utils';
 import { TicketerFrame } from './TicketerFrame';
 
 export const TicketerLeaderboard: React.FC = () => {
   const range = useChartDateRange();
 
-  const { data: ticketers } = useTicketHandlerSummary(
+  const { data, isLoading, error } = useTicketHandlerSummary(
     {
       ...range,
       limit: 10,
@@ -18,28 +18,33 @@ export const TicketerLeaderboard: React.FC = () => {
     refetchQueryOptions(),
   );
 
-  if (ticketers?.length === 0) return <NoDataHint />;
-
   return (
-    <LimitedList
-      indicator={() => (
-        <Stack direction="row" justifyContent="flex-end">
-          <Button
-            size="small"
-            endIcon={<ArrowForward />}
-            component={Link}
-            to="/mods/tickets"
-          >
-            See All
-          </Button>
-        </Stack>
-      )}
+    <QueryHint
+      data={data}
+      isLoading={isLoading}
+      error={error}
+      skeleton={Array.from({ length: 5 }).map((_, index) => (
+        <TicketerFrame key={index} />
+      ))}
     >
-      {ticketers
-        ? ticketers?.map((ticketer) => {
-            return <TicketerFrame key={ticketer.userId} summary={ticketer} />;
-          })
-        : Array.from({ length: 5 }).map((_, i) => <TicketerFrame key={i} />)}
-    </LimitedList>
+      <LimitedList
+        indicator={() => (
+          <Stack direction="row" justifyContent="flex-end">
+            <Button
+              size="small"
+              endIcon={<ArrowForward />}
+              component={Link}
+              to="/mods/tickets"
+            >
+              See All
+            </Button>
+          </Stack>
+        )}
+      >
+        {data?.map((ticketer) => {
+          return <TicketerFrame key={ticketer.userId} summary={ticketer} />;
+        })}
+      </LimitedList>
+    </QueryHint>
   );
 };
