09edf89949f9df1393a275955e454d76e232b1dc
clragon <clragon@clynamic.net>
2024-12-20 19:33:21 +0100

    added personal user upload and ticket charts

--- packages/web/src/users/UserProfileCatalog.tsx
+++ packages/web/src/users/UserProfileCatalog.tsx
@@ -1,5 +1,7 @@
 import { UserCard } from '../common';
 import { createLayout, DashboardCatalog } from '../dashboard';
+import { UploadsSeriesByUploaderChart } from '../janitors';
+import { TicketCreatedSeriesByReporterChart } from '../mods';
 
 export const userProfileCatalog: DashboardCatalog = {
   userHead: {
@@ -20,5 +22,45 @@ export const userProfileCatalog: DashboardCatalog = {
       },
     ),
     component: UserCard,
+  },
+  ticketsHandled: {
+    name: 'Tickets Created',
+    layout: createLayout(
+      {
+        minW: 2,
+        maxW: 6,
+        minH: 2,
+        maxH: 5,
+      },
+      {
+        xs: { x: 0, y: 7, w: 4, h: 5 },
+        sm: { x: 0, y: 7, w: 6, h: 5 },
+        md: { x: 2, y: 7, w: 5, h: 5 },
+        lg: { x: 5, y: 0, w: 5, h: 5 },
+        xl: { x: 5, y: 0, w: 5, h: 5 },
+      },
+    ),
+    card: { title: 'Tickets Created' },
+    component: TicketCreatedSeriesByReporterChart,
+  },
+  postsUploaded: {
+    name: 'Posts Uploaded',
+    layout: createLayout(
+      {
+        minW: 2,
+        maxW: 6,
+        minH: 2,
+        maxH: 5,
+      },
+      {
+        xs: { x: 0, y: 7, w: 4, h: 5 },
+        sm: { x: 0, y: 7, w: 6, h: 5 },
+        md: { x: 2, y: 7, w: 5, h: 5 },
+        lg: { x: 5, y: 0, w: 5, h: 5 },
+        xl: { x: 5, y: 0, w: 5, h: 5 },
+      },
+    ),
+    card: { title: 'Posts Uploaded' },
+    component: UploadsSeriesByUploaderChart,
   },
 };
