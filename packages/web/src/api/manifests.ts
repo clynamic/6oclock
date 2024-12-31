27dc44ef94ca6a58f7a475adccc1f964c9925ab9
clragon <clragon@clynamic.net>
2024-12-31 22:55:52 +0100

    increased version

--- packages/web/src/api/manifests.ts
+++ packages/web/src/api/manifests.ts
@@ -3,7 +3,7 @@
  * Do not edit manually.
  * 5-thirty
  * backend data aggregate for 6 o'clock
- * OpenAPI spec version: 0.0.5
+ * OpenAPI spec version: 0.0.6
  */
 import { useMutation, useQuery } from '@tanstack/react-query';
 import type {
