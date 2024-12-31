6cdddb8bd3d0dded66aff3e5dfaa04dc2777f9cd
clragon <clragon@clynamic.net>
2024-12-31 22:55:52 +0100

    increased version

--- packages/web/src/api/model/activitySummary.ts
+++ packages/web/src/api/model/activitySummary.ts
@@ -3,7 +3,7 @@
  * Do not edit manually.
  * 5-thirty
  * backend data aggregate for 6 o'clock
- * OpenAPI spec version: 0.0.5
+ * OpenAPI spec version: 0.0.6
  */
 
 export interface ActivitySummary {
