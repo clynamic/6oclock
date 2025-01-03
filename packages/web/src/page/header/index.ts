d26ac8d9fa72e4e4804b20f0665e1ec1a866217b
clragon <clragon@clynamic.net>
2025-01-03 04:26:39 +0100

    updated performance links

--- packages/web/src/page/header/index.ts
+++ packages/web/src/page/header/index.ts
@@ -1,5 +1,7 @@
+export * from './href';
 export * from './NavButton';
 export * from './NavDivider';
+export * from './NavLink';
 export * from './NavSpacer';
 export * from './NavUser';
 export * from './PageHeader';
