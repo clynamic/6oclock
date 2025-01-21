8b1f29fce91021e5dd378280677b9191c71d78c1
clragon <clragon@clynamic.net>
2025-01-21 03:46:02 +0100

    fixed health link not being selected

--- packages/web/src/page/header/NavHealth.tsx
+++ packages/web/src/page/header/NavHealth.tsx
@@ -1,8 +1,19 @@
 import { useIsAdmin } from '../../api';
 import { NavLink } from './NavLink';
+import { usePageHeaderContext } from './PageHeaderContext';
 
+// TODO: this is super janky. Nav entries should have dynamic visibility
 export const NavHealth: React.FC = () => {
   const { data: isAdmin } = useIsAdmin();
+  const { currentLink } = usePageHeaderContext();
 
-  return isAdmin ? <NavLink href="/health" label={'Health'} /> : <> </>;
+  return isAdmin ? (
+    <NavLink
+      href="/health"
+      label={'Health'}
+      selected={currentLink?.href === '/health'}
+    />
+  ) : (
+    <> </>
+  );
 };
