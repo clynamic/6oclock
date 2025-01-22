40ee9fe995fed9de18b18422a5208e8d52b7eae4
clragon <clragon@clynamic.net>
2025-01-22 20:16:15 +0100

    removed old code

--- packages/web/src/utils/index.ts
+++ packages/web/src/utils/index.ts
@@ -2,7 +2,6 @@ export * from './activity';
 export * from './breakpoints';
 export * from './charts';
 export * from './months';
-export * from './nest';
 export * from './numbers';
 export * from './query';
 export * from './series';
