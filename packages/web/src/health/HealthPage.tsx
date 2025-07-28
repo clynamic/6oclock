bd60bdba8d0f959dfd3e5b919f022df8bcbe76e2
clragon <clragon@clynamic.net>
2025-07-28 19:18:35 +0200

    updated health dashboard

--- packages/web/src/health/HealthPage.tsx
+++ packages/web/src/health/HealthPage.tsx
@@ -1,54 +1,37 @@
-import { Masonry } from '@mui/lab';
-import { Box, Card, Stack, Typography } from '@mui/material';
-
+import {
+  buildCatalogLayouts,
+  DashboardBody,
+  DashboardEditHeader,
+  DashboardProvider,
+} from '../dashboard';
 import { Page, PageBody, PageFooter, PageHeader, PageTitle } from '../page';
-import { JobsDisplay } from './jobs';
-import { ManifestHealthDisplay } from './manifests';
+import { healthCatalog } from './catalog';
 
 export const HealthPage: React.FC = () => {
   return (
-    <Page>
-      <PageTitle subtitle="Health" />
-      <PageHeader />
-      <PageBody>
-        <Box sx={{ p: 1, width: '100%', height: '100%' }}>
-          <Masonry
-            columns={{ xs: 1, sm: 2, md: 3 }}
-            spacing={2}
-            sx={{
-              alignSelf: 'start',
-            }}
-          >
-            <Card
-              variant="outlined"
-              sx={{
-                background: 'transparent',
-                height: '100%',
-                maxHeight: 700,
-              }}
-            >
-              <Stack p={2} spacing={2} sx={{ height: '100%' }}>
-                <Typography variant="h5">Manifests</Typography>
-                <ManifestHealthDisplay />
-              </Stack>
-            </Card>
-            <Card
-              variant="outlined"
-              sx={{
-                background: 'transparent',
-                height: '100%',
-                maxHeight: 700,
-              }}
-            >
-              <Stack p={2} spacing={2} sx={{ height: '100%' }}>
-                <Typography variant="h5">Jobs</Typography>
-                <JobsDisplay />
-              </Stack>
-            </Card>
-          </Masonry>
-        </Box>
-      </PageBody>
-      <PageFooter />
-    </Page>
+    <DashboardProvider
+      catalog={healthCatalog}
+      data={{
+        version: 1,
+        positions: buildCatalogLayouts(healthCatalog),
+        meta: {},
+      }}
+    >
+      <Page>
+        <PageTitle subtitle="Health" />
+        <PageHeader
+          actions={[
+            (import.meta.env.MODE === 'development' && [
+              <DashboardEditHeader key="edit-dashboard" />,
+            ]) ||
+              [],
+          ]}
+        />
+        <PageBody>
+          <DashboardBody />
+        </PageBody>
+        <PageFooter />
+      </Page>
+    </DashboardProvider>
   );
 };
--- packages/web/src/health/index.ts
+++ packages/web/src/health/index.ts
@@ -1,3 +1,4 @@
+export * from './catalog';
 export * from './jobs';
 export * from './manifests';
 export * from './HealthPage';
