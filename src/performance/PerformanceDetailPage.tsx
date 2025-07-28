fc1a412da509a7de6f6d619571fe51d649ddfb6a
clragon <clragon@clynamic.net>
2025-07-28 15:53:01 +0200

    fixed scrolling on performance pages

--- src/performance/PerformanceDetailPage.tsx
+++ src/performance/PerformanceDetailPage.tsx
@@ -74,221 +74,208 @@ export const PerformanceDetailPage: React.FC = () => {
         actions={[<NavLink href={`/users/${userId}`} label="Profile" />]}
       />
       <PageBody>
-        <Box sx={{ width: '100%', maxWidth: 800, margin: 'auto', p: 2 }}>
-          <Stack sx={{ height: '100%', width: '100%', gap: 1 }}>
-            <Stack direction="column">
-              <Card>
-                <Box p={4} sx={{ width: '100%' }}>
-                  <QueryHint error={error} isEmpty={!summary && !isLoading}>
-                    <Stack direction="column" spacing={2}>
-                      <Stack direction="row" spacing={2}>
-                        <UserAvatar
-                          user={
-                            summary
-                              ? { id: summary.userId, ...summary.userHead }
-                              : undefined
-                          }
-                          size={90}
-                          shape="rounded"
-                        />
-                        <Stack direction="row" spacing={1} sx={{ flexGrow: 1 }}>
-                          <Stack
-                            direction="column"
-                            spacing={1}
-                            sx={{ flexGrow: 1 }}
-                          >
-                            <Stack direction="column">
-                              <UsernameText
-                                user={summary?.userHead}
-                                variant="h4"
-                              />
-                              <Typography variant="h5">
-                                {summary ? (
-                                  summary?.userHead?.level
-                                ) : (
-                                  <Skeleton variant="text" width={100} />
-                                )}
-                              </Typography>
-                              <Typography
-                                variant="caption"
-                                color="text.secondary"
-                              >
-                                {`${range.startDate.toLocaleDateString()} - ${range.endDate.toLocaleDateString()}`}
-                              </Typography>
-                            </Stack>
-                          </Stack>
-                          {summary ? (
-                            <Stack
-                              direction="column"
-                              alignItems={'center'}
-                              gap={2}
-                            >
-                              <Stamp
-                                size={90}
-                                color={getScoreGradeColor(summary.scoreGrade)}
-                              >
-                                {summary.scoreGrade}
-                              </Stamp>
-                            </Stack>
-                          ) : (
-                            <Skeleton
-                              variant="circular"
-                              width={90}
-                              height={90}
-                            />
-                          )}
+        <Box
+          sx={{
+            p: 2,
+            overflowX: 'auto',
+            maxWidth: 800,
+            width: '100%',
+          }}
+        >
+          <Card sx={{ minWidth: 550 }}>
+            <Box p={4} sx={{ width: '100%' }}>
+              <QueryHint error={error} isEmpty={!summary && !isLoading}>
+                <Stack direction="column" spacing={2}>
+                  <Stack direction="row" spacing={2}>
+                    <UserAvatar
+                      user={
+                        summary
+                          ? { id: summary.userId, ...summary.userHead }
+                          : undefined
+                      }
+                      size={90}
+                      shape="rounded"
+                    />
+                    <Stack direction="row" spacing={1} sx={{ flexGrow: 1 }}>
+                      <Stack
+                        direction="column"
+                        spacing={1}
+                        sx={{ flexGrow: 1 }}
+                      >
+                        <Stack direction="column">
+                          <UsernameText user={summary?.userHead} variant="h4" />
+                          <Typography variant="h5">
+                            {summary ? (
+                              summary?.userHead?.level
+                            ) : (
+                              <Skeleton variant="text" width={100} />
+                            )}
+                          </Typography>
+                          <Typography variant="caption" color="text.secondary">
+                            {`${range.startDate.toLocaleDateString()} - ${range.endDate.toLocaleDateString()}`}
+                          </Typography>
                         </Stack>
                       </Stack>
-                      <Stack
-                        direction="row"
-                        justifyContent={'space-between'}
-                        gap={2}
+                      {summary ? (
+                        <Stack direction="column" alignItems={'center'} gap={2}>
+                          <Stamp
+                            size={90}
+                            color={getScoreGradeColor(summary.scoreGrade)}
+                          >
+                            {summary.scoreGrade}
+                          </Stamp>
+                        </Stack>
+                      ) : (
+                        <Skeleton variant="circular" width={90} height={90} />
+                      )}
+                    </Stack>
+                  </Stack>
+                  <Stack
+                    direction="row"
+                    justifyContent={'space-between'}
+                    gap={2}
+                    sx={{
+                      alignSelf: 'center',
+                      width: 400,
+                    }}
+                  >
+                    <Stack direction="column" alignItems="center">
+                      {summary ? (
+                        <RankingText rank={summary.position} variant="h4">
+                          #{summary.position}
+                        </RankingText>
+                      ) : (
+                        <Typography variant="h4">
+                          <Skeleton variant="text" width={60} />
+                        </Typography>
+                      )}
+                      <Typography variant="subtitle1">Rank</Typography>
+                    </Stack>
+                    <Divider orientation="vertical" flexItem />
+                    <Stack direction="column" alignItems="center">
+                      <Typography
+                        variant="h4"
                         sx={{
-                          alignSelf: 'center',
-                          width: 400,
+                          color: getScoreGradeColor(summary?.scoreGrade),
                         }}
                       >
-                        <Stack direction="column" alignItems="center">
-                          {summary ? (
-                            <RankingText rank={summary.position} variant="h4">
-                              #{summary.position}
-                            </RankingText>
-                          ) : (
-                            <Typography variant="h4">
-                              <Skeleton variant="text" width={60} />
-                            </Typography>
-                          )}
-                          <Typography variant="subtitle1">Rank</Typography>
-                        </Stack>
-                        <Divider orientation="vertical" flexItem />
-                        <Stack direction="column" alignItems="center">
-                          <Typography
-                            variant="h4"
-                            sx={{
-                              color: getScoreGradeColor(summary?.scoreGrade),
-                            }}
-                          >
-                            {summary ? (
-                              summary.score
-                            ) : (
-                              <Skeleton variant="text" width={60} />
-                            )}
-                          </Typography>
-                          <Typography variant="subtitle1">Score</Typography>
-                        </Stack>
-                        <Divider orientation="vertical" flexItem />
-                        <Stack direction="column" alignItems="center">
-                          <Typography
-                            variant="h4"
-                            sx={{
-                              color: getTrendGradeColor(summary?.trendGrade),
-                            }}
-                          >
-                            {summary ? (
-                              <TrendIcon grade={summary.trendGrade} size={36} />
-                            ) : (
-                              <Skeleton variant="text" width={60} />
-                            )}
-                          </Typography>
-                          <Typography variant="subtitle1">Trend</Typography>
-                        </Stack>
-                      </Stack>
-                      <Stack direction="row" gap={2}>
-                        <Stack
-                          direction="column"
+                        {summary ? (
+                          summary.score
+                        ) : (
+                          <Skeleton variant="text" width={60} />
+                        )}
+                      </Typography>
+                      <Typography variant="subtitle1">Score</Typography>
+                    </Stack>
+                    <Divider orientation="vertical" flexItem />
+                    <Stack direction="column" alignItems="center">
+                      <Typography
+                        variant="h4"
+                        sx={{
+                          color: getTrendGradeColor(summary?.trendGrade),
+                        }}
+                      >
+                        {summary ? (
+                          <TrendIcon grade={summary.trendGrade} size={36} />
+                        ) : (
+                          <Skeleton variant="text" width={60} />
+                        )}
+                      </Typography>
+                      <Typography variant="subtitle1">Trend</Typography>
+                    </Stack>
+                  </Stack>
+                  <Stack direction="row" gap={2}>
+                    <Stack
+                      direction="column"
+                      sx={{
+                        flex: 1,
+                      }}
+                    >
+                      <Typography variant="h6" sx={{ userSelect: 'none' }}>
+                        Scores
+                      </Typography>
+                      {summary ? (
+                        <SparkLineChart
+                          data={[...summary.history]
+                            .reverse()
+                            .map((e) => e.score)}
+                          area
+                          height={116}
+                          showTooltip
+                          showHighlight
+                          colors={[theme.palette.primary.main]}
                           sx={{
-                            flex: 1,
+                            '.MuiAreaElement-root': {
+                              fill: theme.palette.primary.main,
+                              opacity: 0.3,
+                            },
                           }}
-                        >
-                          <Typography variant="h6" sx={{ userSelect: 'none' }}>
-                            Scores
-                          </Typography>
-                          {summary ? (
-                            <SparkLineChart
-                              data={[...summary.history]
-                                .reverse()
-                                .map((e) => e.score)}
-                              area
-                              height={116}
-                              showTooltip
-                              showHighlight
-                              colors={[theme.palette.primary.main]}
-                              sx={{
-                                '.MuiAreaElement-root': {
-                                  fill: theme.palette.primary.main,
-                                  opacity: 0.3,
-                                },
-                              }}
-                            />
-                          ) : (
-                            <Skeleton variant="rectangular" height={116} />
-                          )}
-                        </Stack>
+                        />
+                      ) : (
+                        <Skeleton variant="rectangular" height={116} />
+                      )}
+                    </Stack>
+                    <Stack
+                      direction="column"
+                      spacing={1}
+                      sx={{
+                        flex: 1,
+                      }}
+                    >
+                      <Typography variant="h6">Stats</Typography>
+                      {summary ? (
                         <Stack
-                          direction="column"
-                          spacing={1}
-                          sx={{
-                            flex: 1,
-                          }}
+                          direction="row"
+                          gap={1}
+                          justifyContent="space-between"
                         >
-                          <Typography variant="h6">Stats</Typography>
-                          {summary ? (
-                            <Stack
-                              direction="row"
-                              gap={1}
-                              justifyContent="space-between"
-                            >
-                              <Typography variant="caption">
-                                {`Active Days`}
-                              </Typography>
-                              <Typography variant="caption">
-                                {`${summary.days}`}
-                              </Typography>
-                            </Stack>
-                          ) : (
-                            <></>
-                          )}
-                          <Stack direction="column" spacing={1}>
-                            {summary
-                              ? Object.entries(summary.activity).map(
-                                  ([type, value]) => (
-                                    <Stack
-                                      key={type}
-                                      direction="row"
-                                      gap={1}
-                                      justifyContent="space-between"
-                                    >
-                                      <Typography variant="caption">
-                                        {getActivityName(
-                                          getActivityFromKey(type),
-                                        )}
-                                      </Typography>
-                                      <Typography variant="caption">
-                                        {formatNumber(value)}
-                                      </Typography>
-                                    </Stack>
-                                  ),
-                                )
-                              : Array.from({ length: 3 }).map((_, index) => (
-                                  <Stack
-                                    key={index}
-                                    direction="row"
-                                    gap={1}
-                                    justifyContent="space-between"
-                                  >
-                                    <Skeleton variant="text" width={100} />
-                                    <Skeleton variant="text" width={30} />
-                                  </Stack>
-                                ))}
-                          </Stack>
+                          <Typography variant="caption">
+                            {`Active Days`}
+                          </Typography>
+                          <Typography variant="caption">
+                            {`${summary.days}`}
+                          </Typography>
                         </Stack>
+                      ) : (
+                        <></>
+                      )}
+                      <Stack direction="column" spacing={1}>
+                        {summary
+                          ? Object.entries(summary.activity).map(
+                              ([type, value]) => (
+                                <Stack
+                                  key={type}
+                                  direction="row"
+                                  gap={1}
+                                  justifyContent="space-between"
+                                >
+                                  <Typography variant="caption">
+                                    {getActivityName(getActivityFromKey(type))}
+                                  </Typography>
+                                  <Typography variant="caption">
+                                    {formatNumber(value)}
+                                  </Typography>
+                                </Stack>
+                              ),
+                            )
+                          : Array.from({ length: 3 }).map((_, index) => (
+                              <Stack
+                                key={index}
+                                direction="row"
+                                gap={1}
+                                justifyContent="space-between"
+                              >
+                                <Skeleton variant="text" width={100} />
+                                <Skeleton variant="text" width={30} />
+                              </Stack>
+                            ))}
                       </Stack>
                     </Stack>
-                  </QueryHint>
-                </Box>
-              </Card>
-            </Stack>
-          </Stack>
+                  </Stack>
+                </Stack>
+              </QueryHint>
+            </Box>
+          </Card>
         </Box>
       </PageBody>
       <PageFooter />
