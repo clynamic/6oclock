c82dbf1cb277be535567a669e7086894a1ed9874
clragon <clragon@clynamic.net>
2025-07-29 18:35:18 +0200

    improved screenshot printer

--- src/performance/PerformanceTable.tsx
+++ src/performance/PerformanceTable.tsx
@@ -8,23 +8,21 @@ import {
   TableHead,
   TableRow,
   Typography,
-  useTheme,
 } from '@mui/material';
-import html2canvas from '@wtto00/html2canvas';
 import { addDays, addMonths, addWeeks, addYears, format } from 'date-fns';
 import { mix } from 'polished';
-import React, { PropsWithChildren, useEffect, useMemo, useRef } from 'react';
+import React, { PropsWithChildren, useMemo } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 import { PerformanceGrade, TrendGrade, usePerformance } from '../api';
 import {
   QueryHint,
   RankingText,
+  ScreenshotPrinter,
   TrendIcon,
   UserAvatar,
   UsernameText,
 } from '../common';
-import { AXIOS_INSTANCE } from '../http';
 import {
   NavButton,
   NavSpacer,
@@ -46,26 +44,6 @@ import {
   useChartValue,
 } from '../utils';
 import { useGradeColors } from './color';
-
-const ProxyImageWrapper: React.FC<PropsWithChildren> = ({ children }) => {
-  const wrapperRef = useRef<HTMLDivElement>(null);
-
-  useEffect(() => {
-    if (!wrapperRef.current) return;
-    const images = wrapperRef.current.querySelectorAll<HTMLImageElement>('img');
-
-    images.forEach((image) => {
-      const newSrc = image.src.replace(
-        'https://static1.e621.net',
-        `${AXIOS_INSTANCE.defaults.baseURL!}/proxy`,
-      );
-
-      image.setAttribute('src', newSrc);
-    });
-  }, []);
-
-  return <Box ref={wrapperRef}>{children}</Box>;
-};
 
 const SpaceCell: React.FC = () => (
   <TableCell>
@@ -113,9 +91,6 @@ export const PerformanceTable: React.FC = () => {
     [range.startDate, range.endDate],
   );
   const navigate = useNavigate();
-
-  const theme = useTheme();
-  const screenshotRef = React.useRef<HTMLDivElement>(null);
 
   const { data, isLoading, error } = usePerformance(
     {
@@ -177,195 +152,165 @@ export const PerformanceTable: React.FC = () => {
     return Array.from(activities).sort();
   }, [data]);
 
-  const handlePrint = () => {
-    if (screenshotRef.current) {
-      html2canvas(screenshotRef.current, {
-        useCORS: true,
-        backgroundColor: theme.palette.background.paper,
-        onclone: (clonedDocument) => {
-          clonedDocument.querySelectorAll<HTMLElement>('p').forEach((el) => {
-            const bg = window.getComputedStyle(el).backgroundImage;
-            if (bg.includes('linear-gradient')) {
-              el.style.color =
-                bg
-                  .match(
-                    /#[0-9a-fA-F]+|rgba?\([\d\s,]+\)|hsla?\([\d\s%,]+\)/,
-                  )?.[0]
-                  ?.trim() || 'currentColor';
-              el.style.backgroundImage = 'none';
-            }
-          });
-
-          Object.assign(
-            clonedDocument.getElementById('screenshot-root')!.style,
-            {
-              backgroundColor: theme.palette.background.paper,
-              backgroundImage: `url("/assets/hex-texture.png")`,
-              backgroundRepeat: 'repeat-x',
-              backgroundPosition: 'top left',
-            },
-          );
-        },
-      }).then((canvas) => {
-        const url = canvas.toDataURL('image/png');
-        const a = document.createElement('a');
-        a.href = url;
-        a.download = `${area}-performance-${format(
-          range.startDate,
-          'yyyy-MM-dd',
-        )}.png`;
-        a.click();
-      });
-    }
-  };
-
   return (
     <Page>
       <PageTitle subtitle="Performance" />
-      <PageHeader
-        actions={[
-          <NavSpacer />,
-          <NavButton key="print" onClick={handlePrint}>
-            Print
-          </NavButton>,
-        ]}
-      />
-      <PageBody>
-        <Box
-          sx={{
-            p: 2,
-            overflowX: 'auto',
-            maxWidth: '100%',
-          }}
-        >
-          <QueryHint data={data} isLoading={isLoading} error={error}>
-            <Stack
-              ref={screenshotRef}
-              id="screenshot-root"
-              sx={{
-                p: 2,
-                width: 'fit-content',
-              }}
-            >
-              <Typography variant="h6">
-                {formatRangeLabel(
-                  range.startDate,
-                  range.endDate,
-                  chartDuration,
-                )}
-              </Typography>
-              <Table size="small" sx={{ width: 'fit-content' }}>
-                <TableHead>
-                  <TableRow>
-                    <SpaceCell />
-                    <SpaceCell />
-                    <TableCell>Name</TableCell>
-                    <SpaceCell />
-                    {activities.sort().map((type) => (
-                      <TableCell key={type}>
-                        {getActivityNoun(getActivityFromKey(type))}
-                      </TableCell>
-                    ))}
-                    <SpaceCell />
-                    {[...periods.slice(1)].reverse().map((period) => (
-                      <TableCell key={period}>{period}</TableCell>
-                    ))}
-                    <SpaceCell />
-                    <TableCell>{periods[0]}</TableCell>
-                    <TableCell>Grade</TableCell>
-                    <TableCell>Trend</TableCell>
-                    <TableCell>Trend</TableCell>
-                  </TableRow>
-                </TableHead>
-                <TableBody>
-                  {data?.map((summary) => (
-                    <TableRow
-                      key={summary.userId}
-                      hover
-                      sx={{
-                        cursor: 'pointer',
-                      }}
-                      onClick={() => {
-                        navigate(`/performance/${summary.userId}`);
-                      }}
-                    >
-                      <TableCell
-                        sx={{
-                          userSelect: 'none',
-                        }}
-                      >
-                        <RankingText rank={summary.position} variant="body1">
-                          {`#${summary.position}`}
-                        </RankingText>
-                      </TableCell>
-                      <TableCell>
-                        <ProxyImageWrapper>
-                          <UserAvatar
-                            user={
-                              summary
-                                ? { id: summary.userId, ...summary.userHead }
-                                : undefined
-                            }
-                            size={40}
-                            shape="rounded"
-                          />
-                        </ProxyImageWrapper>
-                      </TableCell>
-                      <TableCell>
-                        <UsernameText
-                          variant="body1"
-                          user={
-                            summary
-                              ? {
-                                  userId: summary.userId,
-                                  head: summary.userHead,
-                                }
-                              : undefined
-                          }
-                        />
-                      </TableCell>
-                      <SpaceCell />
-                      {activities.sort().map((type) => (
-                        <TableCell key={type} align="center">
-                          {formatNumber(
-                            summary.activity[
-                              type as keyof typeof summary.activity
-                            ] ?? 0,
-                          )}
-                        </TableCell>
-                      ))}
-                      <SpaceCell />
-                      {[...summary.history.slice(1)]
-                        .reverse()
-                        .map((record, i) => (
-                          <GradeCell
-                            grade={record.grade}
-                            key={`${summary.userId}-${i}`}
+      <ScreenshotPrinter
+        filename={`${area}-performance-${format(range.startDate, 'yyyy-MM-dd')}`}
+        targetId="performance-table-root"
+      >
+        {(handlePrint) => (
+          <>
+            <PageHeader
+              actions={[
+                <NavSpacer />,
+                <NavButton key="print" onClick={handlePrint}>
+                  Print
+                </NavButton>,
+              ]}
+            />
+            <PageBody>
+              <Box
+                sx={{
+                  p: 2,
+                  overflowX: 'auto',
+                  maxWidth: '100%',
+                }}
+              >
+                <QueryHint data={data} isLoading={isLoading} error={error}>
+                  <Stack
+                    sx={{
+                      p: 2,
+                      width: 'fit-content',
+                    }}
+                    id="performance-table-root"
+                  >
+                    <Typography variant="h6">
+                      {formatRangeLabel(
+                        range.startDate,
+                        range.endDate,
+                        chartDuration,
+                      )}
+                    </Typography>
+                    <Table size="small" sx={{ width: 'fit-content' }}>
+                      <TableHead>
+                        <TableRow>
+                          <SpaceCell />
+                          <SpaceCell />
+                          <TableCell>Name</TableCell>
+                          <SpaceCell />
+                          {activities.sort().map((type) => (
+                            <TableCell key={type}>
+                              {getActivityNoun(getActivityFromKey(type))}
+                            </TableCell>
+                          ))}
+                          <SpaceCell />
+                          {[...periods.slice(1)].reverse().map((period) => (
+                            <TableCell key={period}>{period}</TableCell>
+                          ))}
+                          <SpaceCell />
+                          <TableCell>{periods[0]}</TableCell>
+                          <TableCell>Grade</TableCell>
+                          <TableCell>Trend</TableCell>
+                          <TableCell>Trend</TableCell>
+                        </TableRow>
+                      </TableHead>
+                      <TableBody>
+                        {data?.map((summary) => (
+                          <TableRow
+                            key={summary.userId}
+                            hover
+                            sx={{
+                              cursor: 'pointer',
+                            }}
+                            onClick={() => {
+                              navigate(`/performance/${summary.userId}`);
+                            }}
                           >
-                            {record.score}
-                          </GradeCell>
+                            <TableCell
+                              sx={{
+                                userSelect: 'none',
+                              }}
+                            >
+                              <RankingText
+                                rank={summary.position}
+                                variant="body1"
+                              >
+                                {`#${summary.position}`}
+                              </RankingText>
+                            </TableCell>
+                            <TableCell>
+                              <UserAvatar
+                                user={
+                                  summary
+                                    ? {
+                                        id: summary.userId,
+                                        ...summary.userHead,
+                                      }
+                                    : undefined
+                                }
+                                size={40}
+                                shape="rounded"
+                              />
+                            </TableCell>
+                            <TableCell>
+                              <UsernameText
+                                variant="body1"
+                                user={
+                                  summary
+                                    ? {
+                                        userId: summary.userId,
+                                        head: summary.userHead,
+                                      }
+                                    : undefined
+                                }
+                              />
+                            </TableCell>
+                            <SpaceCell />
+                            {activities.sort().map((type) => (
+                              <TableCell key={type} align="center">
+                                {formatNumber(
+                                  summary.activity[
+                                    type as keyof typeof summary.activity
+                                  ] ?? 0,
+                                )}
+                              </TableCell>
+                            ))}
+                            <SpaceCell />
+                            {[...summary.history.slice(1)]
+                              .reverse()
+                              .map((record, i) => (
+                                <GradeCell
+                                  grade={record.grade}
+                                  key={`${summary.userId}-${i}`}
+                                >
+                                  {record.score}
+                                </GradeCell>
+                              ))}
+                            <SpaceCell />
+                            <GradeCell grade={summary.scoreGrade}>
+                              {formatNumber(summary.score)}
+                            </GradeCell>
+                            <GradeCell grade={summary.scoreGrade}>
+                              {summary.scoreGrade}
+                            </GradeCell>
+                            <GradeCell grade={summary.trendGrade}>
+                              <TrendIcon grade={summary.trendGrade} />
+                            </GradeCell>
+                            <GradeCell grade={summary.trendGrade}>
+                              {summary.trend}
+                            </GradeCell>
+                          </TableRow>
                         ))}
-                      <SpaceCell />
-                      <GradeCell grade={summary.scoreGrade}>
-                        {formatNumber(summary.score)}
-                      </GradeCell>
-                      <GradeCell grade={summary.scoreGrade}>
-                        {summary.scoreGrade}
-                      </GradeCell>
-                      <GradeCell grade={summary.trendGrade}>
-                        <TrendIcon grade={summary.trendGrade} />
-                      </GradeCell>
-                      <GradeCell grade={summary.trendGrade}>
-                        {summary.trend}
-                      </GradeCell>
-                    </TableRow>
-                  ))}
-                </TableBody>
-              </Table>
-            </Stack>
-          </QueryHint>
-        </Box>
-      </PageBody>
+                      </TableBody>
+                    </Table>
+                  </Stack>
+                </QueryHint>
+              </Box>
+            </PageBody>
+          </>
+        )}
+      </ScreenshotPrinter>
       <PageFooter />
     </Page>
   );
