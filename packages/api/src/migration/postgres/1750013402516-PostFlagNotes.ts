9d7d3d226f1961ec69d7e49e59c13a891f2855c4
clragon <clragon@clynamic.net>
2025-08-06 01:53:11 +0200

    formatted migration files

--- packages/api/src/migration/postgres/1750013402516-PostFlagNotes.ts
+++ packages/api/src/migration/postgres/1750013402516-PostFlagNotes.ts
@@ -1,14 +1,13 @@
-import { MigrationInterface, QueryRunner } from "typeorm";
+import { MigrationInterface, QueryRunner } from 'typeorm';
 
 export class PostFlagNotes1750013402516 implements MigrationInterface {
-    name = 'PostFlagNotes1750013402516'
+  name = 'PostFlagNotes1750013402516';
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "flags" ADD "note" text`);
-    }
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`ALTER TABLE "flags" ADD "note" text`);
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "flags" DROP COLUMN "note"`);
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`ALTER TABLE "flags" DROP COLUMN "note"`);
+  }
 }
