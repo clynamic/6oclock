c884fae597d70f03822df4758c1f413afab5c8ec
clragon <clragon@clynamic.net>
2025-08-05 19:46:20 +0200

    updated deletions to use post events

--- packages/api/src/deletion/metric/deletion-metric.module.ts
+++ packages/api/src/deletion/metric/deletion-metric.module.ts
@@ -1,12 +1,12 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { FlagEntity } from 'src/flag/flag.entity';
+import { PostEventEntity } from 'src/post-event/post-event.entity';
 
 import { DeletionMetricController } from './deletion-metric.controller';
 import { DeletionMetricService } from './deletion-metric.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([FlagEntity])],
+  imports: [TypeOrmModule.forFeature([PostEventEntity])],
   providers: [DeletionMetricService],
   controllers: [DeletionMetricController],
 })
