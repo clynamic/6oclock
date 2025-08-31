ac709fd80140234ff06f617f9a2e15b9944e3c67
clragon <clragon@clynamic.net>
2025-08-31 23:41:38 +0200

    updated performance stamp

--- packages/web/src/common/Stamp.tsx
+++ packages/web/src/common/Stamp.tsx
@@ -1,6 +1,8 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 
 import { Box, Typography } from '@mui/material';
+
+import { generateRosetteSVG } from '../utils/medal';
 
 interface StampProps {
   children?: React.ReactNode;
@@ -9,12 +11,27 @@ interface StampProps {
 }
 
 export const Stamp: React.FC<StampProps> = ({ children, color, size }) => {
+  const stampSize = size ?? 180;
+
+  const svgContent = useMemo(() => {
+    return generateRosetteSVG({
+      size: stampSize + 32,
+      outerR: stampSize / 2 + 8,
+      innerR: stampSize / 2 - 8,
+      biteR: 11,
+      depth: 10,
+      count: 16,
+      bg: 'none',
+      ringColor: color || 'currentColor',
+    });
+  }, [stampSize, color]);
+
   return (
     <Box
       sx={{
         position: 'relative',
 
-        width: size ?? '180px',
+        width: stampSize,
         aspectRatio: 1,
 
         display: 'flex',
@@ -30,17 +47,17 @@ export const Stamp: React.FC<StampProps> = ({ children, color, size }) => {
           right: -16,
           bottom: -16,
           aspectRatio: '1',
-          '--g':
-            '/calc(16.324% + 4.103px) calc(16.324% + 4.103px) radial-gradient(#0000 calc(70% - 5px),#000 calc(71% - 5px) 70%,#0000 71%) no-repeat',
-          mask: 'radial-gradient(100% 100%,#000 calc((16.324% + 4.103px)/0.398 - 12.568px),#0000 calc((16.324% + 4.103px)/0.398 - 12.568px + 1px)) intersect,100% 50% var(--g),96.194% 69.134% var(--g),85.355% 85.355% var(--g),69.134% 96.194% var(--g),50% 100% var(--g),30.866% 96.194% var(--g),14.645% 85.355% var(--g),3.806% 69.134% var(--g),0% 50% var(--g),3.806% 30.866% var(--g),14.645% 14.645% var(--g),30.866% 3.806% var(--g),50% 0% var(--g),69.134% 3.806% var(--g),85.355% 14.645% var(--g),96.194% 30.866% var(--g)',
-          backgroundColor: color,
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
         }}
+        dangerouslySetInnerHTML={{ __html: svgContent }}
       />
       <Typography
         sx={{
           color: color,
           lineHeight: '100%',
-          fontSize: size ? size / 3 : '60px',
+          fontSize: stampSize / 3,
         }}
       >
         {children}
