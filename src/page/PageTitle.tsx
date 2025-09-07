9b14687361b5c87aeb8938e0230c765b7c18c4b8
clragon <clragon@clynamic.net>
2025-09-07 17:50:01 +0200

    improved loading split

--- src/page/PageTitle.tsx
+++ src/page/PageTitle.tsx
@@ -7,7 +7,11 @@ export interface WindowTitleProps {
 
 export const PageTitle: React.FC<WindowTitleProps> = ({ title, subtitle }) => {
   useEffect(() => {
-    document.title = title ? `${title}` : `${subtitle} - 6 o'clock`;
+    document.title = title
+      ? `${title}`
+      : subtitle
+        ? `${subtitle} - 6 o'clock`
+        : "6 o'clock";
   }, [subtitle, title]);
 
   return null;
