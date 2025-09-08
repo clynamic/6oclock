ce046e20f1c25ed5f5cc0fe3109eb6fb7a2a867f
clragon <clragon@clynamic.net>
2025-09-08 20:45:09 +0200

    improved user profile charts

--- packages/web/src/profile/ProfilePage.tsx
+++ packages/web/src/profile/ProfilePage.tsx
@@ -2,7 +2,6 @@ import { useMemo } from 'react';
 
 import { useParams } from 'react-router-dom';
 
-import { adminProfileCatalog } from '../admins/profile/AdminProfileCatalog';
 import { UserArea, useUserHead } from '../api';
 import { DashboardBody } from '../dashboard/DashboardBody';
 import { DashboardProvider } from '../dashboard/DashboardContext';
@@ -10,15 +9,14 @@ import {
   DashboardCatalog,
   buildCatalogLayouts,
 } from '../dashboard/DashboardItem';
-import { janitorProfileCatalog } from '../janitors/profile/JanitorProfileCatalog';
-import { modProfileCatalog } from '../mods/profile/ModProfileCatalog';
 import { Page } from '../page/Page';
 import { PageBody } from '../page/PageBody';
 import { PageFooter } from '../page/PageFooter';
 import { PageTitle } from '../page/PageTitle';
-import { userProfileCatalog } from '../users/UserProfileCatalog';
 import { ChartParamsExtraProvider } from '../utils/charts';
+import { profileCatalog } from './ProfileCatalog';
 import { ProfilePageHeader } from './ProfilePageHeader';
+import { useProfileCharts } from './useProfileChartVisibility';
 
 export const ProfilePage: React.FC = () => {
   const { id } = useParams();
@@ -32,21 +30,6 @@ export const ProfilePage: React.FC = () => {
       enabled: !!userId,
     },
   });
-
-  const catalog = useMemo<DashboardCatalog | undefined>(() => {
-    if (!user) return undefined;
-
-    switch (user.level.toLowerCase()) {
-      case 'janitor':
-        return janitorProfileCatalog;
-      case 'moderator':
-        return modProfileCatalog;
-      case 'admin':
-        return adminProfileCatalog;
-      default:
-        return userProfileCatalog;
-    }
-  }, [user]);
 
   const area = useMemo(() => {
     if (!user) return undefined;
@@ -63,20 +46,32 @@ export const ProfilePage: React.FC = () => {
     }
   }, [user]);
 
+  const { chartIds } = useProfileCharts({
+    id: userId ?? 0,
+    ...user,
+  });
+
+  const catalog = useMemo<DashboardCatalog>(
+    () =>
+      chartIds
+        .filter((chartId) => profileCatalog[chartId])
+        .reduce<DashboardCatalog>((acc, chartId) => {
+          acc[chartId] = profileCatalog[chartId];
+          return acc;
+        }, {}),
+    [chartIds],
+  );
+
   return (
     <DashboardProvider
-      catalog={catalog || {}}
+      catalog={catalog}
       isLoading={isLoading}
       isError={isError}
-      data={
-        catalog
-          ? {
-              version: 1,
-              positions: buildCatalogLayouts(catalog),
-              meta: {},
-            }
-          : undefined
-      }
+      data={{
+        version: 1,
+        positions: buildCatalogLayouts(catalog),
+        meta: {},
+      }}
     >
       <ChartParamsExtraProvider params={{ userId: Number(id), area }}>
         <Page>
