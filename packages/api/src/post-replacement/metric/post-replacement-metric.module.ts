5bd0e67603382a7748845b701118e2e7ebbb6de0
clragon <clragon@clynamic.net>
2025-09-09 12:07:15 +0200

    added replacements handled endpoint

--- packages/api/src/post-replacement/metric/post-replacement-metric.module.ts
+++ packages/api/src/post-replacement/metric/post-replacement-metric.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { PostEventEntity } from 'src/post-event/post-event.entity';
 
 import { PostReplacementEntity } from '../post-replacement.entity';
 import { PostReplacementMetricController } from './post-replacement-metric.controller';
 import { PostReplacementMetricService } from './post-replacement-metric.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([PostReplacementEntity])],
+  imports: [TypeOrmModule.forFeature([PostReplacementEntity, PostEventEntity])],
   controllers: [PostReplacementMetricController],
   providers: [PostReplacementMetricService],
 })
