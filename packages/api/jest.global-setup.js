a2069d863a7d5d3b028d49f5a48f27e3303305b7
clragon <clragon@clynamic.net>
2025-12-26 13:58:44 +0100

    updated test env

--- packages/api/jest.global-setup.js
+++ packages/api/jest.global-setup.js
@@ -1,3 +1,5 @@
 module.exports = async () => {
   process.env.TZ = 'UTC';
+  process.env.E621_GLOBAL_USERNAME = 'jest_test_user_placeholder';
+  process.env.E621_GLOBAL_API_KEY = 'abcdef0123456789abcdef0123456789';
 };
