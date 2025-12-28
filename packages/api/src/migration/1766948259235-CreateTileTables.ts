8e771cf2e0df5960565ef273ccabd2ab01b468d5
clragon <clragon@clynamic.net>
2025-12-28 19:59:14 +0100

    added tiling table migrations

---
+++ packages/api/src/migration/1766948259235-CreateTileTables.ts
@@ -0,0 +1,31 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class CreateTileTables1766948259235 implements MigrationInterface {
+  name = 'CreateTileTables1766948259235';
+
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `CREATE TABLE "upload_hourly_tiles" ("time" TIMESTAMP WITH TIME ZONE NOT NULL, "updated_at" TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now(), "count" integer NOT NULL DEFAULT '0', CONSTRAINT "PK_956c68f0e0657b1f8dbab067c0d" PRIMARY KEY ("time"))`,
+    );
+    await queryRunner.query(
+      `CREATE INDEX "IDX_956c68f0e0657b1f8dbab067c0" ON "upload_hourly_tiles" ("time") `,
+    );
+    await queryRunner.query(
+      `CREATE TABLE "post_pending_hourly_tiles" ("time" TIMESTAMP WITH TIME ZONE NOT NULL, "updated_at" TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now(), "count" integer NOT NULL DEFAULT '0', CONSTRAINT "PK_1d110b6bc773b0ce822abf25120" PRIMARY KEY ("time"))`,
+    );
+    await queryRunner.query(
+      `CREATE INDEX "IDX_1d110b6bc773b0ce822abf2512" ON "post_pending_hourly_tiles" ("time") `,
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `DROP INDEX "public"."IDX_1d110b6bc773b0ce822abf2512"`,
+    );
+    await queryRunner.query(`DROP TABLE "post_pending_hourly_tiles"`);
+    await queryRunner.query(
+      `DROP INDEX "public"."IDX_956c68f0e0657b1f8dbab067c0"`,
+    );
+    await queryRunner.query(`DROP TABLE "upload_hourly_tiles"`);
+  }
+}
