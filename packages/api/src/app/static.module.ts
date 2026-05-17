4ea70334d1ce94ed0066c8fd252b0cb93314f4e9
clragon <clragon@clynamic.net>
2026-05-17 21:37:04 +0200

    fixed serve-static exclude for express 5

--- packages/api/src/app/static.module.ts
+++ packages/api/src/app/static.module.ts
@@ -6,7 +6,7 @@ import { join } from 'path';
   imports: [
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..', 'public'),
-      exclude: ['/api/(.*)'],
+      exclude: ['/api/{*path}'],
     }),
   ],
 })
