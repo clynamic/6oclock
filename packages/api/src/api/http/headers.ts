7a711bb269fe85a823d7b31fa6e6fcabb357dfeb
clragon <clragon@clynamic.net>
2026-05-17 20:25:57 +0200

    upgraded prettier sort-imports to v6

--- packages/api/src/api/http/headers.ts
+++ packages/api/src/api/http/headers.ts
@@ -1,8 +1,6 @@
 import { AxiosResponse } from 'axios';
 
-export const getContentType = (
-  response: AxiosResponse,
-): string | undefined => {
+export const getContentType = (response: AxiosResponse): string | undefined => {
   const value = response.headers['content-type'];
   return typeof value === 'string' ? value : undefined;
 };
